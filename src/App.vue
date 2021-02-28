b49c9080405817aaa65e21b14b2a93f8a574376a
Aaron Welsh <aaronwelsh505@gmail.com>
2021-02-28 21:44:03 +0000

    Theme switcher component.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,69 @@
 <template>
-    <transition name="easeInOut" mode="out-in">
-        <router-view />
-    </transition>
+    <div>
+        <transition name="easeInOut" mode="out-in">
+            <router-view />
+        </transition>
+        <div class="switch-theme">
+            <div
+                v-if="themeStyle === 'light'"
+                @click="changeTheme('dark')"
+                class="fas fa-moon tooltip"
+            >
+                <span class="tooltiptext">Toggle dark mode!</span>
+            </div>
+            <div
+                v-else
+                @click="changeTheme('light')"
+                class="fas fa-sun tooltip"
+            >
+                <span class="tooltiptext">Toggle light mode!</span>
+            </div>
+        </div>
+        <toggle-theme :theme="themeStyle" />
+    </div>
 </template>
+<script>
+    import ToggleTheme from './components/ToggleTheme.vue';
+
+    export default {
+        components: {
+            'toggle-theme': ToggleTheme
+        },
+        data() {
+            return {
+                themeStyle: 'light'
+            };
+        },
+        methods: {
+            changeTheme(theme) {
+                this.themeStyle = theme;
+            }
+        }
+    };
+</script>
+<style>
+    .tooltip {
+        position: relative;
+        display: inline-block;
+    }
+    .tooltip .tooltiptext {
+        visibility: hidden;
+        opacity: 0;
+        transition: 0.3s all ease;
+        min-width: 200px;
+        background-color: #0d1117;
+        color: #eee;
+        text-align: center;
+        padding: 10px 0;
+        border-radius: 6px;
+        position: absolute;
+        left: 30px;
+        bottom: -5px;
+        z-index: 1;
+        font-size: 1em;
+    }
+    .tooltip:hover .tooltiptext {
+        visibility: visible;
+        opacity: 1;
+    }
+</style>
---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,80 @@
+<template>
+    <div>
+        <!-- used for testing -->
+        <span id="toggle-theme">{{toggleTheme}}</span>
+    </div>
+</template>
+<script>
+    export default {
+        props: {
+            theme: {
+                type: String,
+                default: 'light'
+            }
+        },
+        methods: {
+            /**
+             * Change and return theme.
+             */
+            changeTheme() {
+                const body = document.getElementsByTagName('body')[0];
+                // Quick way to reset class to prevent duplicates.
+                // Body has no other classes, hence why it's safe to do this.
+                body.classList = '';
+                body.classList.add(this.theme);
+                return this.theme;
+            }
+        },
+        computed: {
+            toggleTheme() {
+                return this.changeTheme();
+            }
+        }
+    };
+</script>
+<style>
+    #toggle-theme {
+        display: none;
+    }
+    body.dark {
+        background-color: #0d1117;
+    }
+    body.dark section.main .home-btn-container .btn-home {
+        background: #16a085;
+        border: 0;
+        color: #eee;
+    }
+    body.dark header.header-home {
+        background-color: #161b22;
+    }
+    body.dark section.generic-page.about-me {
+        background-color: #161b22;
+        box-shadow: none;
+        color: #eee;
+        transition: .2s all ease;
+    }
+    body.dark section.about-me h1 {
+        color: #eee;
+    }
+    body.dark section.about-me .about-left-info a > span.about-social.fab {
+        color: #eee;
+    }
+    body.dark section.about-me .about-left-info a > span.about-social.fab:hover {
+        color: #16a085;
+    }
+    body.dark section.about-me .about-right-info a {
+        color: #eee;
+    }
+    body.dark section.about-me .about-right-info p {
+        color: #eee;
+    }
+    body.dark nav.nav .pure-menu-item a {
+        color: #eee;
+    }
+    body.dark nav.nav .pure-menu-item a.active {
+        color: #16a085;
+    }
+    body.light {
+        background: #eee;
+    }
+</style>
