7a5e728d29a8b5b4a9983e8874e9bc4959998b5e
Aaron Welsh <social@awelsh.dev>
2021-12-28 20:53:08 +0000

    Data from API and err handling.

---
+++ src/components/Error.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="pure-u-1 error-message">
+        <h1>Error - It's not you, it's me.</h1>
+        <p>
+            There was an issue fetching data from my API.
+            Don't worry, you probably aren't missing much.
+        </p>
+        <p>
+            <a href="mailto:contact@awelsh.dev?subject=API%20Error!">Please drop me a line</a>
+            if this issue persists.
+        </p>
+    </div>
+</template>
+
+<style>
+section.generic-page > .error-message > p {
+    padding-bottom: 1em;
+    font-size: 1.2em;
+    line-height: 1.55em;
+    letter-spacing: -.018em;
+}
+
+body.dark section.generic-page > .error-message > p {
+    color: #ffffff;
+}
+body.light section.generic-page > .error-message > p {
+    color: #525252;
+}
+
+body.dark section.generic-page > .error-message > p > a {
+    color: #ffffff;
+    font-weight: bolder;
+}
+
+body.light section.generic-page > .error-message > p > a {
+    color: #525252;
+    font-weight: bolder;
+}
+
+body.dark section.generic-page > .error-message a:hover,
+body.light section.generic-page > .error-message a:hover {
+    color: #16a085;
+    -webkit-transition: color 0.3s ease;
+    -moz-transition: color 0.3s ease;
+    -o-transition: color 0.3s ease;
+    transition: color 0.3s ease;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,81 +1,93 @@
 <template>
-    <div v-if="loaded">
+    <div>
         <router-link to="/about" class="skip-main">Skip to about me page</router-link>
-        <header class="header-home">
-            <div class="pure-g">
+        <div class="pure-g" v-if="APIError">
+            <section class="generic-page">
+                <error />
+            </section>
+        </div>
 
-                <div class="pure-u-1 pure-u-md-24-24">
-                    <div
-                        class="avatar-logo"
-                        role="img"
-                        :aria-label="image.ariaLabel"
-                    >
-                        <img
-                            class="me pure-img"
-                            :src="image.main.src"
-                            :alt="image.main.alt"
-                        />
-                        <img
-                            class="me pure-img image-hover"
-                            :src="image.mainHover.src"
-                            :alt="image.mainHover.alt"
-                        />
-                    </div>
-                </div>
-                <div class="pure-u-1">
-                    <div class="home-intro">
-                        <h1>{{h1}}</h1>
-                        <h2>{{h2}}</h2>
-                        <div class="social-icons">
-                            <a
-                                v-for="x in social"
-                                :key="x.url"
-                                :href="x.url"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                            >
-                                <span aria-hidden :class="x.icon"></span>
-                            </a>
+        <div v-if="loaded && !APIError">
+            <header class="header-home">
+                <div class="pure-g">
+                    <div class="pure-u-1 pure-u-md-24-24">
+                        <div
+                            class="avatar-logo"
+                            role="img"
+                            :aria-label="image.ariaLabel"
+                        >
+                            <img
+                                class="me pure-img"
+                                :src="image.main.src"
+                                :alt="image.main.alt"
+                            />
+                            <img
+                                class="me pure-img image-hover"
+                                :src="image.mainHover.src"
+                                :alt="image.mainHover.alt"
+                            />
                         </div>
-
                     </div>
+                    <div class="pure-u-1">
+                        <div class="home-intro">
+                            <h1>{{h1}}</h1>
+                            <h2>{{h2}}</h2>
+                            <div class="social-icons">
+                                <a
+                                    v-for="x in social"
+                                    :key="x.url"
+                                    :href="x.url"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <span aria-hidden :class="x.icon"></span>
+                                </a>
+                            </div>
+
+                        </div>
+                    </div>
+
                 </div>
 
-            </div>
-
-        </header>
-        <section class="main">
-            <div class="pure-g">
-                <div class="pure-u-1 pure-u-md-24-24">
-                    <div class="home-btn-container">
-                        <router-link
-                        v-for="button in buttons"
-                        :key="button.route"
-                        :to="{path: button.route}"
-                        :aria-label="button.label"
-                        tag="button"
-                        class="btn-home"
-                        @mouseover="handleButtonHover(
-                            true,
-                            button.hover.enabled ? button.hover.text : button.text
-                        )"
-                        @mouseleave="handleButtonHover(false, button.text)"
-                    >
-                        <span v-if="buttonMessage">{{ buttonMessage }}</span>
-                        <span v-else>{{ button.text }}</span>
-                    </router-link>
+            </header>
+            <section class="main">
+                <div class="pure-g">
+                    <div class="pure-u-1 pure-u-md-24-24">
+                        <div class="home-btn-container">
+                            <router-link
+                            v-for="button in buttons"
+                            :key="button.route"
+                            :to="{path: button.route}"
+                            :aria-label="button.label"
+                            tag="button"
+                            class="btn-home"
+                            @mouseover="handleButtonHover(
+                                true,
+                                button.hover.enabled ? button.hover.text : button.text
+                            )"
+                            @mouseleave="handleButtonHover(false, button.text)"
+                        >
+                            <span v-if="buttonMessage">{{ buttonMessage }}</span>
+                            <span v-else>{{ button.text }}</span>
+                        </router-link>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
     </div>
 </template>
 
 <script>
+    import Error from '../components/Error.vue';
 
     export default {
+        components: {
+            error: Error
+        },
         data() {
             return {
+                APIError: false,
                 loaded: false,
                 h1: null,
                 h2: null,
@@ -87,7 +99,7 @@
             };
         },
         async mounted() {
-            await this.getHomeContent();
+            await this.getContent();
         },
         computed: {
             buttonMessage() {
@@ -95,7 +107,7 @@
             }
         },
         methods: {
-            async getHomeContent() {
+            async getContent() {
                 try {
                     const getContent = await fetch(`${this.apiURL}/home`);
                     const {
@@ -114,7 +126,11 @@
                     this.image = image;
                     document.title = `${title} | Aaron Welsh`; // Set DOM title
                 } catch (err) {
-                    console.error(err);
+                    this.APIError = true;
+                    console.error({
+                        message: 'Something went wrong when fetching content. Please try again.',
+                        errMsg: err
+                    });
                 }
             },
             handleButtonHover(state, message) {
